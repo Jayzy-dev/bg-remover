d3b307667158d9d328cb291fc7f2792479bcb2da
Jayzy-dev <ibralmseyef@gmail.com>
2026-06-15 17:41:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
+from rembg import remove
+from PIL import Image
+import io
 import streamlit as st
-from removebg import RemoveBg
-import os
 
-st.title("أداة إزالة الخلفية الاحترافية")
-
-# الحصول على الـ API Key من إعدادات Streamlit الآمنة
-api_key = st.secrets["REMOVE_BG_API_KEY"]
-
-uploaded_file = st.file_uploader("اختر صورة...", type=["jpg", "png"])
-
-if uploaded_file is not None:
-    with open(uploaded_file.name, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    
-    rmbg = RemoveBg(api_key, "error_log.txt")
-    rmbg.remove_background_from_img_file(uploaded_file.name)
-    
-    st.image(f"{uploaded_file.name}_no_bg.png", caption='النتيجة')
+uploaded_file = st.file_uploader("ارفع صورتك", type=["jpg", "png"])
+if uploaded_file:
+    input_image = Image.open(uploaded_file)
+    output_image = remove(input_image) # هذا هو السحر المجاني!
+    st.image(output_image)
